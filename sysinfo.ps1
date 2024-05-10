88b1f7118ed21741405407cd546de3b87a372aa3
Ye Zeiya Shein <yezeiyashein@gmail.com>
2024-05-10 21:14:45 +0630

    Update sysinfo.ps1

--- sysinfo.ps1
+++ sysinfo.ps1
@@ -7,3 +7,4 @@ $output += systeminfo | format-table
 $output += Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store"
 Write-Output $output |clip
 Write-Host "Data Copied to your clipboard, please paste this data in an new paste at paste.offsec.com (don't copy the url!)" -ForegroundColor Green
+Write-Host ""
